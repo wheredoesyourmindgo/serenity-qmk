d562b4d8be845fda25913d8f8ff481919c5a1a12
wheredoesyourmindgo <moemanchu@icloud.com>
2021-05-21 10:05:01 -0700

    Use Del with LOWER layer

--- keyboards/planck/keymaps/wheredoesyourmindgo/keymap.c
+++ keyboards/planck/keymaps/wheredoesyourmindgo/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Having LT(HIGHER,KC_SPC) allows quick toggle select when arrowing with Lower layer
 	[LOWER] = LAYOUT_planck_grid(
         KC_DOT , KC_1         , KC_2         , KC_3         , KC_4         , KC_5     ,         KC_6          , KC_7   , KC_8 , KC_9, KC_0, LALT(KC_BSPC),
-        KC_NO , XOSM_LCTL, XOSM_LALT, XOSM_LGUI, XOSM_LSFT, DF(LOWER),         KC_DEL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_BSPC,
+        KC_DEL , XOSM_LCTL, XOSM_LALT, XOSM_LGUI, XOSM_LSFT, DF(LOWER),         KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_BSPC,
         KC_LSFT  , KC_NO        , OSM(MOD_RALT)      , KC_NO        , VOID        , KC_NO    ,          KC_NO  , KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_RSFT,
         KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT        , DF(BASE)     , KC_ENT   , KC_TAB, LT(HIGHER,KC_SPC),  KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL
     ),
--- users/wheredoesyourmindgo/wheredoesyourmindgo.h
+++ users/wheredoesyourmindgo/wheredoesyourmindgo.h
@@ -69,11 +69,11 @@
 
 
 // Layer declarations
-#if defined HOME_ROW_MODS
-    enum layers { BASE_HRM, BASE, LOWER, MOUR, MEDR_HRM, MEDR, NSL, NSSL, FUNL, OS, WNDW };
-#else
-    enum layers { BASE, BASE_HRM, LOWER, LOW, LOWEST, HIGHER, HIGHER_HRM, HIGH, HIGHEST, OS, WNDW };
-#endif
+// #if defined HOME_ROW_MODS
+//     enum layers { BASE_HRM, BASE, LOWER, MOUR, MEDR_HRM, MEDR, NSL, NSSL, FUNL, OS, WNDW };
+// #else
+enum layers { BASE, BASE_HRM, LOWER, LOW, LOWEST, HIGHER, HIGHER_HRM, HIGH, HIGHEST, OS, WNDW };
+// #endif
 
 
 

5a664b3d48344a3d2f5fe3184602d2bb370a2290
wheredoesyourmindgo <moemanchu@icloud.com>
2021-06-25 10:22:07 -0700

    Added shortcuts to WNDW Layer

--- users/wheredoesyourmindgo/wheredoesyourmindgo.h
+++ users/wheredoesyourmindgo/wheredoesyourmindgo.h
@@ -27,6 +27,8 @@
 #define WNDW_MV_RGHT HYPR(KC_O)     // Move right
 #define WNDW_LFT_TTHRD LCA(KC_E)    // Left two-thirds
 #define WNDW_RGHT_TTHRD LCA(KC_T)   // Right two-thirds
+#define WNDW_LFT_THRD LCA(KC_D)     // Right third
+#define WNDW_RGNT_THRD LCA(KC_G)    // Left third
 #define WNDW_LFT_HLF LCA(KC_LEFT)   // Left half
 #define WNDW_CNTR_HLF HYPR(KC_C)    // Center
 #define WNDW_RGHT_HLF LCA(KC_RGHT)  // Right half

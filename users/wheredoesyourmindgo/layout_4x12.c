a047de3b240a58af2be921647b43ba10d003ef5d
wheredoesyourmindgo <moemanchu@icloud.com>
2021-07-13 08:06:44 -0700

    Swap keycode positions for - and _

--- users/wheredoesyourmindgo/layout_4x12.c
+++ users/wheredoesyourmindgo/layout_4x12.c
@@ -30,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 	[LOW] = LAYOUT_4x12(
         KC_DEL, KC_EXLM      , KC_AT        , KC_HASH      , KC_DLR       , KC_PERC ,                  KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, KC_BSPC,
-        KC_NO, XOSM_LCTL, XOSM_LALT, XOSM_LGUI, XOSM_LSFT, DF(LOW),                  KC_MINS, KC_EQL , KC_LCBR, KC_RCBR, KC_PIPE, KC_QUOT,
-        KC_LSFT, KC_NO, OSM(MOD_RALT), KC_NO        , KC_NO        , VOID   ,                  KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_RSFT,
+        KC_NO, XOSM_LCTL, XOSM_LALT, XOSM_LGUI, XOSM_LSFT, DF(LOW),                  KC_EQL, KC_MINS , KC_LCBR, KC_RCBR, KC_PIPE, KC_QUOT,
+        KC_LSFT, KC_NO, OSM(MOD_RALT), KC_NO        , KC_NO        , VOID   ,                  KC_PLUS, KC_UNDS, KC_LBRC, KC_RBRC, KC_BSLS, KC_RSFT,
         KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT,        KC_NO        , DF(BASE), KC_TAB, KC_SPC,  KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL
     ),
     [LOWEST] = LAYOUT_4x12(

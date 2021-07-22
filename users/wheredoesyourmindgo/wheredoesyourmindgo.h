abadd9b749e6c723ae9102771886dff1c45eec37
Abe Hendricks <ahendricks@pcwa.net>
2021-07-22 16:44:12 -0700

    Added Ristretto layout.

--- users/wheredoesyourmindgo/wheredoesyourmindgo.h
+++ users/wheredoesyourmindgo/wheredoesyourmindgo.h
@@ -152,12 +152,12 @@ enum {
 typedef enum { TD_NONE, TD_INTERRUPTED, TD_NOT_INTERRUPTED } td_state_t;
 
 #if defined PLANCK_KEYBOARD
-#    define LAYOUT_4x12(k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b)      \
+#define LAYOUT_4x12(k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b)      \
         {                                                                                                                                                                                                                                                                    \
             {k00, k01, k02, k03, k04, k05}, {k10, k11, k12, k13, k14, k15}, {k20, k21, k22, k23, k24, k25}, {k30, k31, k32, k39, k3a, k3b}, {k06, k07, k08, k09, k0a, k0b}, {k16, k17, k18, k19, k1a, k1b}, {k26, k27, k28, k29, k2a, k2b}, { k36, k37, k38, k33, k34, k35 } \
         }
 #elif defined TECHNIK_KEYBOARD
-#    define LAYOUT_4x12(K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K40, K41, K42, K43, K44, K45, K46, K47) \
+#define LAYOUT_4x12(K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K40, K41, K42, K43, K44, K45, K46, K47) \
         {                                                                                                                                                                                                                                                               \
             {K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11}, {K12, K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23}, {K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, K34, K35}, { K36, K37, K38, K39, K40, K41, K42, K43, K44, K45, K46, K47 }    \
         }
@@ -175,12 +175,12 @@ typedef enum { TD_NONE, TD_INTERRUPTED, TD_NOT_INTERRUPTED } td_state_t;
 	{ K300,  K301,  K302,  K303,  K304,  K305,  K306,  K307,  K308,  K309,  K310,  K311 } \
 }
 #elif defined SIGNUM_KEYBOARD
-#    define LAYOUT_4x12(K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311) \
+#define LAYOUT_4x12(K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311) \
         {                                                                                                                                                                                                                                                                                                               \
             {K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011}, {K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111}, {K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211}, { K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311 }    \
         }
 #elif defined BOARDWALK_KEYBOARD
-#    define LAYOUT_boardwalk_hs(k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k010, k011, k012, k013, k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k110, k111, k112, k113, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k210, k211, k212, k213, k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k310, k311, k312, k313, k40, k41, k42, k43, k44, k45, k47, k49, k410, k411, k412, k413)      \
+#define LAYOUT_boardwalk_hs(k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k010, k011, k012, k013, k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k110, k111, k112, k113, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k210, k211, k212, k213, k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k310, k311, k312, k313, k40, k41, k42, k43, k44, k45, k47, k49, k410, k411, k412, k413)      \
         {                                                                                                                                                                                                                                                                                                                                                                                                    \
             {k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k010, k011, k012, k013}, {k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k110, k111, k112, k113}, {k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k210, k211, k212, k213}, {k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k310, k311, k312, k313}, { k40, k41, k42, k43, k44, k45, KC_NO, k47, KC_NO, k49, k410, k411, k412, k413 } \
         }

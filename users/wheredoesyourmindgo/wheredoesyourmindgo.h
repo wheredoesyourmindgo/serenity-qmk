6287915ba69ec634075456a36689a8914a9efb66
Abe Hendricks <ahendricks@pcwa.net>
2021-07-22 14:32:49 -0700

    Added switch tabs shortcuts

--- users/wheredoesyourmindgo/wheredoesyourmindgo.h
+++ users/wheredoesyourmindgo/wheredoesyourmindgo.h
@@ -99,6 +99,8 @@
 #define OS_FWD LGUI(KC_RBRC)            // Forward
 #define OS_BCK_FLDR LGUI(KC_UP)         // Back Folder
 #define OS_FWD_FLDR LGUI(KC_DOWN)       // Forward Folder
+#define OS_NXT_TAB LCTL(KC_TAB)         // Next Tab
+#define OS_PRV_TAB LSFT(LCTL(KC_TAB))   // Previous Tab
 // custom (non-default) in OS
 #define OS_SIRI HYPR(KC_S)                   // Siri
 #define OS_LNCHPD HYPR(KC_L)                 // Launchpad
@@ -160,10 +162,18 @@ typedef enum { TD_NONE, TD_INTERRUPTED, TD_NOT_INTERRUPTED } td_state_t;
             {K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11}, {K12, K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23}, {K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, K34, K35}, { K36, K37, K38, K39, K40, K41, K42, K43, K44, K45, K46, K47 }    \
         }
 #elif defined CORNELIUS_KEYBOARD
-#    define LAYOUT_4x12(K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311) \
-        {                                                                                                                                                                                                                                                                                                               \
-            {K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011}, {K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111}, {K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211}, { K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311 }    \
-        }
+#define LAYOUT_4x12( \
+	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
+	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, \
+	K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, \
+	K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311 \
+) \
+{ \
+	{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011 }, \
+	{ K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111 }, \
+	{ K200,  K201,  K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211 }, \
+	{ K300,  K301,  K302,  K303,  K304,  K305,  K306,  K307,  K308,  K309,  K310,  K311 } \
+}
 #elif defined SIGNUM_KEYBOARD
 #    define LAYOUT_4x12(K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311) \
         {                                                                                                                                                                                                                                                                                                               \
@@ -174,6 +184,18 @@ typedef enum { TD_NONE, TD_INTERRUPTED, TD_NOT_INTERRUPTED } td_state_t;
         {                                                                                                                                                                                                                                                                                                                                                                                                    \
             {k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k010, k011, k012, k013}, {k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k110, k111, k112, k113}, {k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k210, k211, k212, k213}, {k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k310, k311, k312, k313}, { k40, k41, k42, k43, k44, k45, KC_NO, k47, KC_NO, k49, k410, k411, k412, k413 } \
         }
+#elif defined RISTRETTO_KEYBOARD
+#define LAYOUT_4x12_enc( \
+		K000, K001, K002, K003, K004, K005, 	  K007, K008, K009, K010, K011, K012, \
+		K100, K101, K102, K103, K104, K105,		  K107, K108, K109, K110, K111, K112, \
+		K200, K201, K202, K203, K204, K205,		  K207, K208, K209, K210, K211, K212, \
+		K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311, K312  \
+) { \
+		{K000, K001, K002, K003, K004, K005, KC_NO, K007, K008, K009, K010, K011, K012}, \
+		{K100, K101, K102, K103, K104, K105, KC_NO, K107, K108, K109, K110, K111, K112}, \
+		{K200, K201, K202, K203, K204, K205, KC_NO, K207, K208, K209, K210, K211, K212}, \
+		{K300, K301, K302, K303, K304, K305, K306 , K307, K308, K309, K310, K311, K312}  \
+}
 #endif
 
 #endif

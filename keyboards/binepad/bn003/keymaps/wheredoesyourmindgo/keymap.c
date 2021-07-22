a0ad1d438362c96534990e48d3fcfb0cefcc3485
Abe Hendricks <ahendricks@pcwa.net>
2021-07-22 10:59:03 -0700

    Added Binepad BN003 layout

---
+++ keyboards/binepad/bn003/keymaps/wheredoesyourmindgo/keymap.c
@@ -0,0 +1,19 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_F22,
+        KC_F23,
+        KC_F24
+    ),
+    [1] = LAYOUT(
+        KC_NO,
+        KC_NO,
+        KC_NO
+    ),
+    [2] = LAYOUT(
+        KC_NO,
+        KC_NO,
+        KC_NO
+    )
+};

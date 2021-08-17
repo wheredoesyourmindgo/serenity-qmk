15a6a09cc5e4b72dcb29137962c9259c0eb9d912
Abe Hendricks <ahendricks@pcwa.net>
2021-08-17 16:36:27 -0700

    Swap visor and spotlight shortcuts

--- users/wheredoesyourmindgo/layout_boardwalk_hs.c
+++ users/wheredoesyourmindgo/layout_boardwalk_hs.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   OS_SCRN_SHT_OPT,  OS_LNCHPD, OS_SPTLGHT,               OS_SIRI,            OS_SDBR,        KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,  OS_DSKTP,       OS_PRV_SPC,     CMD_TAB_PRV,        CMD_TAB_NXT,        OS_NXT_SPC,     OS_FLLSCRN,
         KC_NO, KC_NO, KC_NO, KC_NO, VOID, KC_NO, KC_NO, KC_NO,   PRV_APP,         OS_MSN_CNTRL,   OS_PRVS_APP_WNDW,   OS_NXT_APP_WNDW,    OS_APP_WNDWS,   KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, DF(BASE), DF(BASE),  OS_SPTLGHT,  OS_LNCHR,                     KC_NO,              KC_NO,              KC_NO,          KC_NO
+        KC_NO, KC_NO, KC_NO, KC_NO, DF(BASE), DF(BASE),  OS_TRM_VSR,  OS_LNCHR,                     KC_NO,              KC_NO,              KC_NO,          KC_NO
     )
 };
 

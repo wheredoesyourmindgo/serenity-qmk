52e84c05c512a3c620913a87672889232f8ae7ac
wheredoesyourmindgo <moemanchu@icloud.com>
2022-02-28 09:41:47 -0800

    added comment

--- make_all.sh
+++ make_all.sh
@@ -5,6 +5,7 @@ qmk compile -kb binepad/bn003 -km wheredoesyourmindgo
 qmk compile -kb boardwalk -km wheredoesyourmindgo $OPTS
 qmk compile -kb ristretto -km wheredoesyourmindgo $OPTS
 qmk compile -kb boardsource/technik_o -km wheredoesyourmindgo $OPTS
+# qmk compile -kb boardsource/technik_o -km wheredoesyourmindgo $OPTS -e RGB_MATRIX_ENABLE=yes -e KEY_LOCK_ENABLE=no
 qmk compile -kb vitamins_included/rev2 -km wheredoesyourmindgo $OPTS
 qmk compile -kb foostan/cornelius -km wheredoesyourmindgo $OPTS
 qmk compile -kb signum/3_0/elitec -km wheredoesyourmindgo $OPTS

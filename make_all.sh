5acd065c7bc53780c0f8cb0cf99085c2d07867e2
Abe Hendricks <ahendricks@pcwa.net>
2024-09-03 16:19:14 -0700

    Added Corne elite-c build to make_all.sh

--- make_all.sh
+++ make_all.sh
@@ -10,6 +10,7 @@ qmk compile -kb boardsource/technik_o -km wheredoesyourmindgo $OPTS
 qmk compile -kb vitamins_included/rev2 -km wheredoesyourmindgo $OPTS
 qmk compile -kb foostan/cornelius -km wheredoesyourmindgo $OPTS
 qmk compile -kb crkbd -km wheredoesyourmindgo $OPTS -e CONVERT_TO=elite_pi
+qmk compile -kb crkbd -km wheredoesyourmindgo $OPTS
 qmk compile -kb signum/3_0/elitec -km wheredoesyourmindgo $OPTS
 # qmk compile -kb checkerboards/quark -km wheredoesyourmindgo $OPTS
 # qmk compile -kb planck/rev6 -km wheredoesyourmindgo $OPTS -e ENCODER_ENABLE=yes

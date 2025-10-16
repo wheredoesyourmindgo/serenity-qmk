6babddcde59035c3d8c8c8b7af73ec6ea4be4001
Abe Hendricks <ahendricks@pcwa.net>
2025-10-16 15:17:12 -0700

    - Fix breaking changes - Remove RGB functions from keymaps (temporarily)

--- users/wheredoesyourmindgo/wheredoesyourmindgo.c
+++ users/wheredoesyourmindgo/wheredoesyourmindgo.c
@@ -358,35 +358,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case SHAKE_MOUSE:
             if (record->event.pressed) {
-                register_code16(KC_MS_ACCEL1);
+                register_code16(MS_ACL1);
                 int u;
                 int i;
                 int distance = 4;
                 for (i = 1; i <= distance; ++i) {
-                    tap_code(KC_MS_LEFT);
+                    tap_code(MS_LEFT);
                 }
                 for (u = 1; u <= 6; ++u) {
                     for (i = 1; i <= distance; ++i) {
-                        tap_code(KC_MS_RIGHT);
-                        tap_code(KC_MS_UP);
+                        tap_code(MS_RGHT);
+                        tap_code(MS_UP);
                     }
                     for (i = 1; i <= distance; ++i) {
-                        tap_code(KC_MS_RIGHT);
-                        tap_code(KC_MS_DOWN);
+                        tap_code(MS_RGHT);
+                        tap_code(MS_DOWN);
                     }
                     for (i = 1; i <= distance; ++i) {
-                        tap_code(KC_MS_LEFT);
-                        tap_code(KC_MS_DOWN);
+                        tap_code(MS_LEFT);
+                        tap_code(MS_DOWN);
                     }
                     for (i = 1; i <= distance; ++i) {
-                        tap_code(KC_MS_LEFT);
-                        tap_code(KC_MS_UP);
+                        tap_code(MS_LEFT);
+                        tap_code(MS_UP);
                     }
                 }
                 for (i = 1; i <= distance; ++i) {
-                    tap_code(KC_MS_RIGHT);
+                    tap_code(MS_RGHT);
                 }
-                unregister_code16(KC_MS_ACCEL1);
+                unregister_code16(MS_ACL1);
             }
             break;
         case WNDW_LP_VRT_MAX:

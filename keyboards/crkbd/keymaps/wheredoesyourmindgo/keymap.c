c240f50f9ad7a9354df731aca1b7d74372836b75
Abe Hendricks <ahendricks@pcwa.net>
2025-10-22 10:22:01 -0700

    Serenity - move z and x

--- keyboards/crkbd/keymaps/wheredoesyourmindgo/keymap.c
+++ keyboards/crkbd/keymaps/wheredoesyourmindgo/keymap.c
@@ -5,19 +5,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_L, KC_D, KC_W, KC_V, KC_J, KC_F, KC_O, KC_U, KC_MINS, KC_BSPC,
         LT(MOUSE,KC_ESC), KC_R, KC_N, KC_T, KC_S, KC_G, KC_Y, KC_H, KC_E, KC_A, KC_I, KC_SCLN,
-        KC_LSFT, KC_X, KC_Z, KC_M, KC_C, KC_B, KC_K, KC_P, KC_QUOT, KC_COMM, KC_DOT, RSFT_T(KC_ENT),
+        KC_LSFT, KC_Z, KC_X, KC_M, KC_C, KC_B, KC_K, KC_P, KC_QUOT, KC_COMM, KC_DOT, RSFT_T(KC_ENT),
         MO(AUX), MO(NUMNAV), KC_SPC, KC_SPC, MO(SYMBL), MO(HRDWR)
     ),
     [BASE_L] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_L, KC_D, KC_W, KC_V, KC_J, KC_F, KC_O, KC_U, KC_MINS, KC_BSPC,
         KC_ESC, KC_R, KC_N, KC_T, KC_S, KC_G, KC_Y, KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, KC_SCLN,
-        KC_LSFT, KC_X, KC_Z, KC_M, KC_C, KC_B, KC_K, KC_P, KC_QUOT, KC_COMM, KC_DOT, RSFT_T(KC_ENT),
+        KC_LSFT, KC_Z, KC_X, KC_M, KC_C, KC_B, KC_K, KC_P, KC_QUOT, KC_COMM, KC_DOT, RSFT_T(KC_ENT),
         KC_NO, KC_NO, KC_SPC, KC_SPC, KC_NO, KC_NO
     ),
     [BASE_R] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_L, KC_D, KC_W, KC_V, KC_J, KC_F, KC_O, KC_U, KC_MINS, KC_BSPC,
         KC_ESC,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_G, KC_Y, KC_H, KC_E, KC_A, KC_I, KC_SCLN,
-        KC_LSFT, KC_X, KC_Z, KC_M, KC_C, KC_B, KC_K, KC_P, KC_QUOT, KC_COMM, KC_DOT, RSFT_T(KC_ENT),
+        KC_LSFT, KC_Z, KC_X, KC_M, KC_C, KC_B, KC_K, KC_P, KC_QUOT, KC_COMM, KC_DOT, RSFT_T(KC_ENT),
         KC_NO, KC_NO, KC_SPC, KC_SPC, KC_NO, KC_NO
     ),
     [QWRTY] = LAYOUT_split_3x6_3(

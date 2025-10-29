ee5a5c627d601115b588bb8223ea6c34602e7ebb
Abe Hendricks <ahendricks@pcwa.net>
2025-10-29 11:47:02 -0700

    Add dz60 keymap

--- make_all.sh
+++ make_all.sh
@@ -17,3 +17,4 @@ qmk compile -kb signum/3_0/elitec -km wheredoesyourmindgo $QMK_OPTS
 qmk compile -kb planck/rev6 -km wheredoesyourmindgo $QMK_OPTS
 # qmk compile -kb planck/rev6 -km wheredoesyourmindgo -j 2 -e QWERTY_BASE=no -e ENCODER_ENABLE=no
 qmk compile -kb hhkb/ansi/32u2 -km wheredoesyourmindgo $QMK_OPTS
+qmk compile -kb dztech/dz60rgb_ansi -km wheredoesyourmindgo $QMK_OPTS
